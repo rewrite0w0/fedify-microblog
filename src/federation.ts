5902c2f95c10cc0d72d7885721cae79fcd22a833
OH TAEJUN <rewrite0w0@gmail.com>
2026-08-09 00:41:04 +0900

    feat: 키 쌍 디스패처 구현한다

--- src/federation.ts
+++ src/federation.ts
@@ -1,9 +1,15 @@
-import { createFederation } from "@fedify/fedify";
+import {
+  InProcessMessageQueue,
+  MemoryKvStore,
+  createFederation,
+  exportJwk,
+  generateCryptoKeyPair,
+  importJwk,
+} from "@fedify/fedify";
 import { Endpoints, Person } from "@fedify/vocab";
 import db from "./db.ts";
-import type { Actor, User } from "./schema.ts";
+import type { Actor, Key, User } from "./schema.ts";
 import { getLogger } from "@logtape/logtape";
-import { InProcessMessageQueue, MemoryKvStore } from "@fedify/fedify";
 
 const logger = getLogger("microblog");
 
@@ -12,41 +18,91 @@ const federation = createFederation({
   queue: new InProcessMessageQueue(),
 });
 
-
-//  양쪽에 정의
-
-federation.setActorDispatcher(
-  "/users/{identifier}",
-  async (ctx, identifier) => {
-    try {
+try {
+  federation
+    .setActorDispatcher("/users/{identifier}", async (ctx, identifier) => {
       const user = db
         .prepare<unknown[], User & Actor>(
           `
-  SELECT * FROM users
-  JOIN actors ON (users.id = actors.user_id)
-  WHERE users.username = ?
-  `,
+        SELECT * FROM users
+        JOIN actors ON (users.id = actors.user_id)
+        WHERE users.username = ?
+        `,
         )
         .get(identifier);
-
       if (user == null) return null;
-      logger.info("Dispatching actor {identifier}", { identifier });
+
+      const keys = await ctx.getActorKeyPairs(identifier);
       return new Person({
         id: ctx.getActorUri(identifier),
         preferredUsername: identifier,
-        name: identifier,
+        name: user.name,
         inbox: ctx.getInboxUri(identifier),
         endpoints: new Endpoints({
-          sharedInbox: ctx.getInboxUri()
+          sharedInbox: ctx.getInboxUri(),
         }),
-        url:ctx.getActorUri(identifier)
+        url: ctx.getActorUri(identifier),
+        publicKey: keys[0].cryptographicKey,
+        assertionMethods: keys.map((k) => k.multikey),
       });
-    } catch (e) {
-      console.error(e);
-    }
-  },
-);
+    })
+    .setKeyPairsDispatcher(async (ctx, identifier) => {
+      const user = db
+        .prepare<unknown[], User>("SELECT * FROM users WHERE username = ?")
+        .get(identifier);
+      if (user == null) return [];
+      const rows = db
+        .prepare<unknown[], Key>("SELECT * FROM keys WHERE keys.user_id = ?")
+        .all(user.id);
+      const keys = Object.fromEntries(
+        rows.map((row) => [row.type, row]),
+      ) as Record<Key["type"], Key>;
+      const pairs: CryptoKeyPair[] = [];
+      // 사용자가 지원하는 두 키 형식 (RSASSA-PKCS1-v1_5 및 Ed25519) 각각에 대해
+      // 키 쌍을 보유하고 있는지 확인하고, 없으면 생성 후 데이터베이스에 저장:
+      for (const keyType of ["RSASSA-PKCS1-v1_5", "Ed25519"] as const) {
+        if (keys[keyType] == null) {
+          logger.debug(
+            "The user {identifier} does not have an {keyType} key; creating one...",
+            { identifier, keyType },
+          );
+          const { privateKey, publicKey } =
+            await generateCryptoKeyPair(keyType);
+          db.prepare(
+            `
+          INSERT INTO keys (user_id, type, private_key, public_key)
+          VALUES (?, ?, ?, ?)
+          `,
+          ).run(
+            user.id,
+            keyType,
+            JSON.stringify(await exportJwk(privateKey)),
+            JSON.stringify(await exportJwk(publicKey)),
+          );
+          pairs.push({ privateKey, publicKey });
+        } else {
+          pairs.push({
+            privateKey: await importJwk(
+              JSON.parse(keys[keyType].private_key),
+              "private",
+            ),
+            publicKey: await importJwk(
+              JSON.parse(keys[keyType].public_key),
+              "public",
+            ),
+          });
+        }
+      }
+      return pairs;
+    });
+} catch (e) {
+  console.error(e);
+}
 
-federation.setInboxListeners("/users/{identifier}/inbox", "/inbox");
+try {
+  federation.setInboxListeners("/users/{identifier}/inbox", "/inbox");
+} catch (e) {
+  console.error(e);
+}
 
 export default federation;
--- src/schema.ts
+++ src/schema.ts
@@ -10,7 +10,16 @@ export interface Actor {
   handle: string; // @johndoe@example.com 모양의 연합우주 핸들을 담는다
   name: string | null; // UI에 표시되는 이름을 담는다, 빌 수도 있다.
   inbox_url: string; // 수신함 URL
-  shared_inbox_url: string | null; // 공유 수신함 URL
+  shared_inbox_url: string | null; // 공유 수신함 URL11
   url: string | null; // 액터의 프로필 URL
   created: string; // 렠드가 생성된 시점
 }
+
+export interface Key {
+  user_id: number;
+  type: "RSASSA-PKCS1-v1_5" | "Ed25519";
+  private_key: string;
+  public_key: string;
+  created: string;
+}
+
